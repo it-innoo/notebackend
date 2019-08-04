76663dd6a27a01d988d037c4ecbc811e92bbe69e
jailgreen <36865973+jailgreen@users.noreply.github.com>
2019-08-04 12:36:54 +0300

    Part 3-4

---
+++ README.md
@@ -0,0 +1,3 @@
+# Tietokantaa käyttävä backend
+
+[Note example] (https://dashboard.heroku.com/apps/still-hamlet-40053)
---
+++ build/precache-manifest.62feb572c9db9b043780d8a846988ad8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76bbe9a78b32e050db3f531add451708",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ab060351f0cc2e04a942",
+    "url": "/static/css/main.0729b415.chunk.css"
+  },
+  {
+    "revision": "1af2e68c30103fa82895",
+    "url": "/static/js/2.f3384baa.chunk.js"
+  },
+  {
+    "revision": "ab060351f0cc2e04a942",
+    "url": "/static/js/main.6dfec65e.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
